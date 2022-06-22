7a7e4c8f7dd4882c1df542bf8bc9e382ab097ae7
Marlene Guzman <guzmanmarlene@yahoo.com>
2022-06-21 22:46:11 -0700

    adding a method to Player class that creates tokens

--- js/Player.js
+++ js/Player.js
@@ -4,6 +4,19 @@ class Player {
   this.id = id; 
   this.color = color;
   this.active = active;
-  this.tokens = [];
+  this.tokens = this.createTokens(21);
+  }
+  /**
+ * Creates token objects for player
+ * @param     {number}    num - Number of token objects to be created
+ * @returns   {Array}     An array of the newly created token objects
+ */
+  createTokens(num){
+    const tokens = [];
+      for(let i = 0; i < num.length; i++){
+        let token = new Token(i, this)
+        tokens.push(token)
+      }
+    return tokens
   }
 }
