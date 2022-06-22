688d300e67b9ad42fef7a75c2019f7bfb5e10790
Marlene Guzman <guzmanmarlene@yahoo.com>
2022-06-22 12:01:07 -0700

    rendering Spaces and Board.

--- js/Board.js
+++ js/Board.js
@@ -1,25 +1,35 @@
 class Board {
   constructor() {
-this.rows = 6;
-this.columns = 7;
-this.spaces = this.createSpaces();
+    this.rows = 6;
+    this.columns = 7;
+    this.spaces = this.createSpaces();
   }
-  /** 
- * Generates 2D array of spaces. 
- * @return  {Array}     An array of space objects
- */
-  createSpaces(){
+  /**
+   * Generates 2D array of spaces.
+   * @return  {Array}     An array of space objects
+   */
+  createSpaces() {
     const spaces = [];
 
-    for(let x = 0; x < this.columns; x++) {
+    for (let x = 0; x < this.columns; x++) {
       const column = [];
-    
-      for(let y = 0; y < this.rows; y++){
+
+      for (let y = 0; y < this.rows; y++) {
         const space = new Space(x, y);
-        column.push(space)
+        column.push(space);
       }
-      spaces.push(column)
+      spaces.push(column);
     }
     return spaces;
+  }
+/**
+ * Draws associated SVG spaces for all game spaces.
+ */
+  drawHTMLBoard(){
+    for (let column of this.spaces) {
+      for (let space of column) {
+          space.drawSVGSpace();
+      }
+    }
   }
 }
--- js/Space.js
+++ js/Space.js
@@ -1,9 +1,27 @@
 class Space {
-  constructor(x, y, id, token){
+  constructor(x, y, id, token, diameter, radius){
 this.x = x;
 this.y = y;
 this.id = `space-${x}-${y}`
 this.token = null;
+this.diameter = 76;
+this.radius =  this.radius/2
 // this.tokens = this.createTokens(21);
   }
+/**
+ * Draws SVG space
+ */
+  drawSVGSpace(){
+    const svgSpace = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+    svgSpace.setAttributeNS(null, "id", this.id);
+    svgSpace.setAttributeNS(null, "cx", (this.x * this.diameter) + this.radius);
+    svgSpace.setAttributeNS(null, "cy", (this.y * this.diameter) + this.radius);
+    svgSpace.setAttributeNS(null, "r", this.radius - 8);
+    svgSpace.setAttributeNS(null, "fill", "black");
+    svgSpace.setAttributeNS(null, "stroke", "none");
+  
+    document.getElementById("mask").appendChild(svgSpace);   
+  }
+  setAttributeNS(){}
 }
