c6c53dd7af974e8bcd1f44b6a9db40b03b7220e1
Marlene Guzman <guzmanmarlene@yahoo.com>
2022-06-21 23:37:53 -0700

    adding constructor methods to Game class and createPlayer method

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,16 @@
 class Game {
-  
+  constructor(board, players, ready){
+    this.board = new Board()
+    this.players = this.createPlayers()
+    this.ready = false;
+  }
+  /** 
+ * Creates two player objects
+ * @return  {Array}    An array of two Player objects.
+ */
+  createPlayers(){
+   const players = [new Player('Player 1', 1, 'e15258', true),
+                    new Player('Player 1', 2, '#e59a13')]
+    return players
+  }
 }
